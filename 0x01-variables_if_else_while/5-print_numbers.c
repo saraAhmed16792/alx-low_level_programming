73264dc89f84bd3107e61d8efb73136de7c99981
Sara <sara.a.imbaby@gmail.com>
2023-06-17 05:44:24 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%i", num);
+
+	printf("\n");
+
+	return (0);
+}
+
