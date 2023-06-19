741a93c78a6fa4f567d3e31212c5aefdc96f8ce6
Sara <sara.a.imbaby@gmail.com>
2023-06-19 07:31:51 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int digit1 = 0;
+	int digit2;
+
+	while (digit1 <= 99)
+	{
+		digit2 = digit1;
+		while (digit2 <= 99)
+		{
+			if (digit2 != digit1)
+			{
+				putchar((digit1 / 10) + 48);
+				putchar((digit1 % 10) + 48);
+				putchar(' ');
+				putchar((digit2 / 10) + 48);
+				putchar((digit2 % 10) + 48);
+
+				if (digit1 != 98 || digit2 != 90)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			digit2++;
+		}
+		digit1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
+
