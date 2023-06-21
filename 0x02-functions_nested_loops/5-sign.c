975aafd696cb4ea7436cdb04499de47d3e76121c
Sara <sara.a.imbaby@gmail.com>
2023-06-21 16:20:20 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: number to be checked
- * Return: 1 for positive numbers, -1 for negative number or 0 for anything else 
+ * Return: 1 for positive, -1 for nevative, 0 for anything else 
  */
 
 int print_sign(int n)
