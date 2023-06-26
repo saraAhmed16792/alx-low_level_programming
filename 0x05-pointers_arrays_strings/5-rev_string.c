5b8a113d7d7ce30f9407faec752f553da45217dc
Sara <sara.a.imbaby@gmail.com>
2023-06-26 07:08:37 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,16 +7,16 @@
 
 void rev_string(char *s)
 {
-	int l, i;
+	int l = 0, i = 0;
 	char temp;
 
-	for (1 = 0; s[l] != '\0'; l++)
-		;
+	while (s[i++])
+		l++;
 
-	for (i = 0; i < l / 2; i++)
+	for (i = l - 1; i >= l / 2; i--)
 	{
-		temp = s[i];
-		s[i] = s[l - 1 - i];
-		s[l - 1 - i] = temp;
+		tmp = s[i];
+		s[i] = s[l - i - 1];
+		s[l - i - 1] = tmp;
 	}
 }
