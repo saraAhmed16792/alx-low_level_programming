c42a3adf1ee3bcce776149365b3fee14aa579996
Sara <sara.a.imbaby@gmail.com>
2023-06-30 06:21:29 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,16 +11,15 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
 	j = 0;
-	while (src[i] != '\0')
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-		i++;
 		j++;
+		i++;
 	}
 	dest[i] = '\0';
 	return (dest);
