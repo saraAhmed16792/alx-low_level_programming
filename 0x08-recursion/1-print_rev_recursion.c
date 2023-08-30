84077d4320d797bb077bacbdbabcb1db4148f36f
Sara <sara.a.imbaby@gmail.com>
2023-08-30 06:08:05 -0700

    1-print_rev_recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * __print_rev_recursion - prints a string in reverse.
+ * _print_rev_recursion - prints a string in reverse.
  * @s: string
  */
 void _print_rev_recursion(char *s)
