3b78cf84552d458fa93d002ff8cd95f965746bbe
Sara <sara.a.imbaby@gmail.com>
2023-08-30 08:33:30 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,7 +11,7 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (actual_prime(n, n -1));
+	return (actual_prime(n, n - 1));
 }
 
 /**
@@ -28,4 +28,3 @@ int actual_prime(int n, int i)
 		return (0);
 	return (actual_prime(n, i - 1));
 }
- 
