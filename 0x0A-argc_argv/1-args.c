214825efd2a5e39278a0bbd580362783ca90c818
Sara <sara.a.imbaby@gmail.com>
2023-09-03 10:10:13 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main: print num of argu
+ * @argc: num of argus
+ * @argv: array of argus
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv; /*Ignore argv*/
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
