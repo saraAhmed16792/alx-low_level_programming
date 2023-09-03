7ef038d574e59af29ae73a48fe2cdc957ebc9b57
Sara <sara.a.imbaby@gmail.com>
2023-09-03 10:42:35 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - converts a string to int
+ * @s: string to convert
+ * Return: int converted
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int d = 0;
+	int n = 0;
+	int len = 0;
+	int f = 0;
+	int digit = 0;
+
+	while (s[len] != '\0')
+	{
+		if (s[i] == '-')
+			++d;
+		
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+/**
+ * main - multiplies 2 nums
+ * @argc: num of argus
+ * @argv: array og argus
+ * Return: 0 if it correct , 1 if wrong
+ */
+int main(int argc, char *argv[])
+{
+	int result, n1, n2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	n1 = _atoi(argv[1]);
+	n2 = _atoi(argv[2]);
+	result = n1 * n2;
+
+	printf("%d", result);
+
+	return (0);
+}
