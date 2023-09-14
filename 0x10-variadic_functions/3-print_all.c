c654c1340a998e265b974eda5cb6d7d8e4f9eeac
Sara <sara.a.imbaby@gmail.com>
2023-09-14 11:00:29 -0700

    2-print_strings.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,7 +4,7 @@
 
 /**
  * print_all - prints anything
- * format: is a list of types of arguments passed to the function
+ * @format: is a list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
