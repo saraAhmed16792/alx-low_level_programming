3f929f017313534c06660f9d9a303e7f9ffea2ec
Sara <sara.a.imbaby@gmail.com>
2023-10-03 09:46:30 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,7 @@
  * create_file - create afile
  * @filename: is the name of the file to create
  * @text_content: is a NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure 
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
