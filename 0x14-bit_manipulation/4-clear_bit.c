730eb57d59f6dcaa4a266f8e030fd799068e777c
Sara <sara.a.imbaby@gmail.com>
2023-12-06 10:25:14 -0800

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,11 +7,8 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int setbit;
-
-	if (index > (sizeof(unsigned long int) * 8 - 1))
+	if (index > sizeof(n) * 8)
 		return (-1);
-	setbit = 1 << index;
-	*n = *n | setbit;
+	*n &= ~(1 << index);
 	return (1);
 }
