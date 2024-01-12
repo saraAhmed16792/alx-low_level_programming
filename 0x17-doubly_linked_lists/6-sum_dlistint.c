6455553623d554917f64c7368f010e51996b7315
Sara <sara.a.imbaby@gmail.com>
2024-01-12 00:35:32 -0800

    6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 /**
- * sum_dlistint -  returns the sum of all the data (n) of a dlistint_t linked list.
+ * sum_dlistint - returns the sum of all the data (n) of
+ * a dlistint_t linked listi.
  * @head: head of the list
  * Return: if the list is empty, return 0
  */
