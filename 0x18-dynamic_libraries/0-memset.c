747843fd2474f0b9daf168d8b44033b151fc918d
Sara <sara.a.imbaby@gmail.com>
2024-01-15 06:31:00 -0800

    README.md

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - memory
+ * @s: ptr to char params
+ * @b: data to change
+ * @n: idx
+ * Return: *s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
